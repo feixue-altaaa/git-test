24315268c2d9aa37e55f33317af7be49bafe9aa3
Qls <qls11235813@163.com>
2022-04-19 20:40:47 +0800

    third version

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -4,6 +4,7 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("git hello!!");
         System.out.println("git hello2!!");
+        System.out.println("git hello3!!");
     }
 
 }
